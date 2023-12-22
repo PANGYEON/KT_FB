711b267329a7f116a1fd0c284d5c713dd5245438
hnlee <hn.lee7618@gmail.com>
2023-12-22 17:22:01 +0900

    프로필화면수정

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { Button, View, Text, Divider, HStack, Box, Modal } from 'native-base';
+import { Button, View, Text, Divider, HStack, Box, Modal, Input } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 // import {Pressable}  from 'react-native';
 // import {Modal}  from 'react-native-modal';
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
+
   const [logoutModalVisible, setLogoutModalVisible] = useState(false);
   const [dietGoalModalVisible, setDietGoalModalVisible] = useState(false);
   const [activityLevelModalVisible, setactivityLevelModalVisible] = useState(false);
-
+  const [isEditing, setIsEditing] = useState(false);
 
   const logout = () => {
     // 로그아웃 로직
@@ -25,6 +26,12 @@ const ProfileScreen = () => {
     dietgoal: '유지',
     activitylevel: '2레벨 - 주 3~4회 이하, 움직임 조금 있는 직종',
   });
+
+  const [updatedName, setUpdatedName] = useState(userInfo.name);
+  const [updatedBirthDate, setUpdatedBirthDate] = useState(userInfo.birthDate);
+  const [updatedHeight, setUpdatedHeight] = useState(userInfo.height);
+  const [updatedWeight, setUpdatedWeight] = useState(userInfo.weight);
+  const [updatedGender, setUpdatedGender] = useState(userInfo.gender);
   // 다이어트 목표 변경 함수
   const changeDietGoal = (newGoal) => {
     setUserInfo({ ...userInfo, dietgoal: newGoal });
@@ -35,10 +42,68 @@ const ProfileScreen = () => {
     setUserInfo({ ...userInfo, activitylevel: newLevel });
     setactivityLevelModalVisible(false);
   };
+
+  // 이전 버튼 눌렀을 때 처리
+  const goBack = () => {
+    // 변경된 활동 레벨과 식단 목표를 파라미터로 전달하여 이전 화면으로 돌아감
+    navigation.navigate('BottomTabNavigator', {
+      updatedActivityLevel : userInfo.activitylevel,
+      updatedDietGoal : userInfo.dietgoal,
+    });
+  };
+
+  // // '수정하기' 버튼을 눌렀을 때 모달을 열기 위한 함수
+  // const openEditModal = () => {
+  //   setUpdatedName(userInfo.name);
+  //   setEditModalVisible(true);
+  // };
+
+  // // 모달을 닫기 위한 함수
+  // const closeEditModal = () => {
+  //   setEditModalVisible(false);
+  // };
+
+  // 수정된 사용자 정보를 저장하는 함수
+  // const saveUserInfo = (updatedInfo) => {
+  //   setUserInfo({ ...userInfo, ...updatedInfo });
+  //   closeEditModal();
+  // };
+
+  // const activateNameEdit = () => {
+  //   setIsEditingName(true);
+  // };
+
+  // const deactivateNameEdit = () => {
+  //   setIsEditingName(false);
+  //   setUpdatedName(userInfo.name);
+  // };
+
+  // 이름 저장을 위한 함수
+  // const saveUserInfo = () => {
+  //   setUserInfo({ ...userInfo, name : updatedName }); // 이름 업데이트
+  //   setUserInfo({ ...userInfo, birthDate : updatedBirthDate }); // 생년월일 업데이트
+  //   setUserInfo({ ...userInfo, height : updatedHeight }); // 키 업데이트
+  //   setUserInfo({ ...userInfo, weight : updatedWeight }); // 몸무게 업데이트
+  //   setUserInfo({ ...userInfo, gender : updatedGender }); // 성별 업데이트
+  //   setIsEditing(false); // 수정모드 종료
+  // }
+
+  const saveUserInfo = () => {
+    setUserInfo({
+      ...userInfo,
+      name: updatedName,
+      birthDate: updatedBirthDate,
+      height: updatedHeight,
+      weight: updatedWeight,
+      gender: updatedGender,
+    });
+    setIsEditing(false); // 수정모드 종료
+  };
+
   return (
     <View>
       {/* 이전 버튼 */}
-      <Button onPress={() => navigation.navigate("Home")} position="absolute" top={4} left={4}>
+      <Button onPress={goBack} position="absolute" top={4} left={4}>
         이전
       </Button>
 
@@ -52,18 +117,60 @@ const ProfileScreen = () => {
       {/* 계정 정보 및 수정 버튼 */}
       <HStack space={2} alignItems="center">
         <Text>계정 정보</Text>
-        <Button size="sm">수정하기</Button>
+        {/* <Button size="sm" onPress = {() => setIsEditingName(true)}>수정하기</Button> */}
+        {!isEditing ? (
+          <Button size="sm" onPress={() => setIsEditing(true)}>수정하기</Button>
+          ) : 
+          (<>
+            <Button onPress={saveUserInfo}>저장</Button>
+            <Button onPress={() => setIsEditing(false)}>취소</Button>
+          </>)
+        }
       </HStack>
       <Divider my={4} />
 
-      {/* 유저 정보 표시 */}
-      <Box>
-        <Text>이름: {userInfo.name}</Text>
-        <Text>생년월일: {userInfo.birthDate}</Text>
-        <Text>키: {userInfo.height}</Text>
-        <Text>몸무게: {userInfo.weight}</Text>
-        <Text>성별: {userInfo.gender}</Text>
-      </Box>
+      {!isEditing ? (
+        <Box>
+          <Text>이름: {userInfo.name}</Text>
+          <Text>생년월일: {userInfo.birthDate}</Text>
+          <Text>키: {userInfo.height}cm</Text>
+          <Text>몸무게: {userInfo.weight}kg</Text>
+          <Text>성별: {userInfo.gender}</Text>
+        </Box>
+      ) : (
+        <Box>
+          <Input
+            placeholder="이름"
+            onChangeText={(text) => setUpdatedName(text)}
+            value={updatedName}
+          />
+
+          <Input
+            placeholder="생년월일"
+            onChangeText={(text) => setUpdatedBirthDate(text)}
+            value={updatedBirthDate}
+          />
+
+          <Input
+            placeholder="키"
+            onChangeText={(text) => setUpdatedHeight(text)}
+            value={updatedHeight}
+          />
+
+          <Input
+            placeholder="몸무게"
+            onChangeText={(text) => setUpdatedWeight(text)}
+            value={updatedWeight}
+          />
+
+          <Input
+            placeholder="성별"
+            onChangeText={(text) => setUpdatedGender(text)}
+            value={updatedGender}
+          />
+          {/* 다른 수정 가능한 사용자 정보 입력 필드 추가 */}
+        </Box>
+      )}
 
       {/* 건강 정보 및 수정 안내 */}
       <HStack space={2} alignItems="center" mt={4}>
@@ -71,6 +178,7 @@ const ProfileScreen = () => {
         <Text>클릭해서 수정하세요</Text>
       </HStack>
       <Divider my={4} />
+
       {/* 기타 버튼들 */}
 
       <Button mt={4} onPress={() => setactivityLevelModalVisible(true)}>
