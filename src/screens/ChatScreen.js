78ef2154412c230ca6ac831c0a92c300d80bff65
ujum <juj0927@gmail.com>
2023-12-22 17:54:07 +0900

    채팅 스타일

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,6 +1,6 @@
 // ChatScreen.js
 import React, { useState } from 'react';
-import { Button, View, Input, Text, VStack, ScrollView } from 'native-base';
+import { Button, View, Input, Text, VStack,HStack, ScrollView } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 
 const ChatScreen = () => {
@@ -8,44 +8,48 @@ const ChatScreen = () => {
   const [inputText, setInputText] = useState(''); // To store the user input
   const [chatHistory, setChatHistory] = useState([]); // To store the chat history
 
-  // Function to handle sending a message
   const handleSendMessage = () => {
     if (inputText.trim() === '') return;
 
-    // Add the user's message to chat history
     setChatHistory([...chatHistory, { role: 'user', content: inputText }]);
-
-    // Call your chatGPT function here and get the response
-    // For now, we'll just mock a response
-    const mockResponse = "This is a mock response from the chatbot.";
-    setChatHistory(currentChat => [...currentChat, { role: 'bot', content: mockResponse }]);
-
-    // Clear the input field
+    const mockResponse = "chatbot 답변";
+    setChatHistory(currentChat => [...currentChat, { role: '너는 식단 관리자야.', content: mockResponse }]);
     setInputText('');
   };
 
   return (
     <View flex={1}>
+      <Button onPress={() => navigation.navigate("Home")}>이전</Button>
       <ScrollView>
         {chatHistory.map((msg, index) => (
-          <Text key={index} bold={msg.role === 'user'}>
-            {msg.role === 'user' ? 'You: ' : 'Bot: '}
-            {msg.content}
-          </Text>
+          <Text
+            key={index}
+            style={{
+              alignSelf: msg.role === 'user' ? 'flex-end' : 'flex-start',
+              backgroundColor: msg.role === 'user' ? '#e1e1e1' : '#d1d1f1',
+              borderRadius: 10,
+              padding: 10,
+              marginVertical: 2,
+              maxWidth: '80%'
+          }}>
+          {msg.content}
+        </Text>
         ))}
       </ScrollView>
 
       <VStack space={3} marginBottom="5">
-        <Input
-          placeholder="Type a message..."
-          value={inputText}
-          onChangeText={setInputText}
-          onSubmitEditing={handleSendMessage}
-        />
-        <Button onPress={handleSendMessage}>Send</Button>
+        <HStack space={2} alignItems="center">
+          <Input
+            flex={1}
+            placeholder="메시지를 입력하세요..."
+            value={inputText}
+            onChangeText={setInputText}
+            onSubmitEditing={handleSendMessage}
+          />
+          <Button onPress={handleSendMessage}>보내기</Button>
+        </HStack>
       </VStack>
 
-      <Button onPress={() => navigation.navigate("Home")}>이전</Button>
     </View>
   );
 };
