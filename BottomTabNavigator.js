9d2b390da9f0a1813d95edbc56b653e0176eb043
ujum <juj0927@gmail.com>
2024-01-03 13:48:08 +0900

    chat->friend

--- BottomTabNavigator.js
+++ BottomTabNavigator.js
@@ -8,7 +8,7 @@ import ChatScreen from './src/screens/ChatScreen';
 import HomeIcon from './src/icons/HomeIcon.png';
 import ReportIcon from './src/icons/ReportIcon.png';
 import BoardIcon from './src/icons/BoardIcon.png';
-import ChatIcon from './src/icons/ChatIcon.png';
+import FriendIcon from './src/icons/FriendIcon.png';
 import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
@@ -19,7 +19,7 @@ const BottomTabNavigator = () => {
       <Tab.Screen name="홈" component={HomeScreen} options={{ headerShown: false , tabBarIcon: () => (<Image source={HomeIcon} style={{ width: 25, height: 25 }} />)}}/>
       <Tab.Screen name="리포트" component={ReportScreen} options={{ headerShown: false , tabBarIcon: () => (<Image source={ReportIcon} style={{ width: 25, height: 25 }} />)}}/>
       <Tab.Screen name="게시판" component={BoardScreen} options={{ headerShown: false , tabBarIcon: () => (<Image source={BoardIcon} style={{ width: 25, height: 25 }} />)}}/>
-      <Tab.Screen name="채팅" component={ChatScreen} options={{ headerShown: false , tabBarIcon: () => (<Image source={ChatIcon} style={{ width: 25, height: 25 }} />)}}/>
+      <Tab.Screen name="친구" component={ChatScreen} options={{ headerShown: false , tabBarIcon: () => (<Image source={FriendIcon} style={{ width: 25, height: 25 }} />)}}/>
     </Tab.Navigator>
   );
 };
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -8,27 +8,12 @@ import { ChatIcon } from '../icons/ChatIcon.png';
 
 const ChatScreen = () => {
   const navigation = useNavigation();
-  const [inputText, setInputText] = useState(''); // To store the user input
-  const [chatHistory, setChatHistory] = useState([]); // To store the chat history
-  const scrollViewRef = useRef();
 
   const [friends, setFriends] = useState(['친구1', '친구2', '친구3']);
   const [selectedFriend, setSelectedFriend] = useState(null);
 
   const selectFriend = (friend) => {
     setSelectedFriend(friend);
-  };
-
-  const handleSendMessage = async() => {
-    if (inputText.trim() === '') return;
-    
-    const newUserMessage = { role: 'user', content: inputText };
-    setChatHistory(prevChatHistory => [...prevChatHistory, newUserMessage]);
-    setInputText('');
-
-    // 친구 식단 받아오기 뭐 그런거
-    const friendMessage = { role: 'friend', content: '친구 채팅' };
-    setChatHistory(prevChatHistory => [...prevChatHistory, friendMessage]);
   };
 
   return (
@@ -48,7 +33,7 @@ const ChatScreen = () => {
         </ScrollView>
       </View>
 
-      {/* 채팅방 */}
+      {/* 구독 친구 식단 */}
       <View style={{ flex: 1 , backgroundColor:'#fff'}}>
       <View flex={1}>
       <HStack 
@@ -64,62 +49,9 @@ const ChatScreen = () => {
         <View>
           <Image source={ChatIcon} style={{width:25, height:25}} />
         </View>
-        {selectedFriend && <Text>{selectedFriend}과의 대화</Text>}
+        {selectedFriend && <Text>{selectedFriend}의 식단</Text>}
       </HStack>
-      <ScrollView
-        onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}
-        ref={scrollViewRef}
-      >
-        {chatHistory.map((msg, index) => (
-          <Text
-            key={index}
-            style={{
-              fontSize:16,
-              alignSelf: msg.role === 'user' ? 'flex-end' : 'flex-start',
-              backgroundColor: msg.role === 'user' ? '#8E86FA' : '#EBEBEB',
-              borderRadius: 22,
-              paddingVertical:10,
-              paddingHorizontal:15,
-              marginHorizontal:20,
-              marginVertical: 5,
-              maxWidth: '80%',
-              shadowColor: "#000",
-              shadowOffset: { width: 0, height: 2 },
-              shadowOpacity: 0.25,
-              shadowRadius: 3.84,
-              elevation: 5,
-          }}>
-          {msg.content}
-        </Text>
-        ))}
-      </ScrollView>
-
-      <VStack space={3} marginBottom="5" style={{borderWidth:0}}>
-        <HStack space={2} alignItems="center" style={{margin:10, borderWidth:0, borderTopWidth:1, borderTopColor:'#D9D9D9', paddingTop:15}}>
-          <Input
-            flex={1}
-            placeholder="메시지를 입력하세요..."
-            value={inputText}
-            onChangeText={setInputText}
-            onSubmitEditing={handleSendMessage}
-            style={{ 
-              backgroundColor: '#fff',
-              borderRadius: 11,
-            }}
-          />
-          <Button onPress={handleSendMessage} 
-                  style={{ 
-                    backgroundColor: '#fff',
-                    borderRadius: 11,
-                    shadowColor: "#000",
-                    shadowOffset: { width: 0, height: 2 },
-                    shadowOpacity: 0.25,
-                    shadowRadius: 3.84,
-                    elevation: 5,
-                  }}
-          ><Text style={{ color: 'grey' }}>보내기</Text></Button>
-        </HStack>
-      </VStack>
+      
     </View>
     </View>
     </View>
