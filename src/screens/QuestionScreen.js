eab3e33b568ef7a0ed9a43ece333ce598cdb8a6a
ujum <juj0927@gmail.com>
2024-01-09 20:37:04 +0900

    1:1 문의 css, rendering

--- src/screens/QuestionScreen.js
+++ src/screens/QuestionScreen.js
@@ -1,12 +1,13 @@
 
 import React, { useState, useEffect } from 'react';
 // import { View, Text, FlatList, TouchableOpacity, Image, Modal, StyleSheet } from 'react-native';
-import { View, Text, FlatList, TouchableOpacity, Image, Modal, TextInput, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image, TextInput, StyleSheet, Button, Modal } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import BackIcon from '../icons/BackIcon.png';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { HStack} from 'native-base';
 
 const QuestionScreen = () => {
   const [questions, setQuestions] = useState([]);
@@ -16,6 +17,14 @@ const QuestionScreen = () => {
   const [newQuestionTitle, setNewQuestionTitle] = useState('');
   const [newQuestionContent, setNewQuestionContent] = useState('');
   const navigation = useNavigation();
+
+  // 문의 제목, 내용 확인 Alert
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+
+  // 삭제하시겠습니까? Alert
+  const [showDeleteConfirmModal, setShowDeleteConfirmModal] = useState(false);
+  const [deleteQuestionId, setDeleteQuestionId] = useState(null);
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -47,40 +56,62 @@ const QuestionScreen = () => {
 
     return (
       <Modal
-        animationType="slide"
+        animationType="fade"
         transparent={true}
         visible={showModal}
         onRequestClose={() => setShowModal(false)}
       >
+        <View style={styles.modalOverlay}>
         <View style={styles.modalView}>
-        <Text style={{fontSize:20}}>문의 제목 : {selectedQuestion.title}</Text>
-          <Text style={{fontSize:17}}>문의 내용 : {selectedQuestion.content}</Text>
+        <TouchableOpacity
+          onPress={() => setShowModal(false)}  // 입력 필드 초기화
+          style={{
+            position: 'absolute',
+            top: 10,
+            right: 10,
+            padding: 10,
+            zIndex: 1 // 버튼이 다른 요소 위에 표시되도록 zIndex 설정
+          }}>
+          <Text>X</Text>
+        </TouchableOpacity>
+        <Text style={{fontSize:20}}> 문의 제목 : {selectedQuestion.title} </Text>
+          <Text style={{fontSize:17}}> 문의 내용 : {selectedQuestion.content} </Text>
+
+          <View style={styles.separator} />
           
           {selectedQuestion.answers.length > 0 ? (
             selectedQuestion.answers.map((answer, index) => (
-              <Text key={index}>답변 : {answer.content}</Text> // 예시로 answer.content를 사용했습니다. 실제 구조에 맞게 조정하세요.
+              <Text key={index}> 답변 : {answer.content} </Text> // 예시로 answer.content를 사용했습니다. 실제 구조에 맞게 조정하세요.
             ))
           ) : (
-            <Text>답변이 아직 안왔습니다..</Text>
+            <Text> 답변이 아직 안왔습니다.. </Text>
           )}
           <TouchableOpacity
           style={styles.deleteButton}
           onPress={() => handleDeleteQuestion(selectedQuestion.id)}
         >
-          <Text>삭제</Text>
+          <Text style={{color:'#fff'}}> 삭제 </Text>
         </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.closeButton}
-            onPress={() => setShowModal(false)}
-          >
-            <Text >닫기</Text>
-          </TouchableOpacity>
+        </View>
         </View>
       </Modal>
     );
   };
 
-  const handleCreateQuestion = async () => {
+  const handleCreateQuestion = () => {
+    if (!newQuestionTitle) {
+      setAlertMessage('제목을 입력해주세요');
+      return;
+    }
+    if (!newQuestionContent) {
+      setAlertMessage('내용을 입력해주세요');
+      return;
+    }
+    setShowConfirmModal(true);
+  };
+
+  const confirmCreateQuestion = async () => {
+    // 기존의 handleCreateQuestion 로직을 여기로 옮깁니다.
     const token = await AsyncStorage.getItem('@user_token');
     const data = new FormData();
     data.append('title', newQuestionTitle);
@@ -96,64 +127,186 @@ const QuestionScreen = () => {
     };
   
     try {
-       await axios(config);
+      const response = await axios(config);
       // 여기서 추가된 문의를 처리하세요. 예를 들어, 문의 목록을 다시 불러올 수 있습니다.
+      const newQuestion = response.data; // API 응답에서 새로운 문의 데이터를 받아옵니다.
       setShowQuestionModal(false);
+      setQuestions([newQuestion, ...questions]); // 새 문의를 목록의 앞에 추가합니다.
+      setShowQuestionModal(false);
+      setNewQuestionTitle(''); // 입력 필드 초기화
+      setNewQuestionContent(''); // 입력 필드 초기화 
     } catch (error) {
       console.error(error);
       console.log(error.response);
     }
+
+    setShowConfirmModal(false); // 등록 확인 모달 닫기
   };
-  const handleDeleteQuestion = async (questionId) => {
+
+  const renderAlertModal = () => (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={!!alertMessage}
+      onRequestClose={() => setAlertMessage('')}
+    >
+      <View style={styles.modalOverlay}>
+        <View style={styles.modalView}>
+          <Text>{alertMessage}</Text>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => setAlertMessage('')}
+          >
+            <Text style={{color: '#fff'}}>닫기</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+
+  const renderConfirmModal = () => (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={showConfirmModal}
+      onRequestClose={() => setShowConfirmModal(false)}
+    >
+      <View style={styles.modalOverlay}>
+        <View style={styles.modalView}>
+          <View style={{ alignSelf: 'flex-start' }}> 
+            <Text style={{textAlign:'left', fontSize:16, color:'black'}}>등록하시겠습니까? </Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonNo]}
+              onPress={() => setShowConfirmModal(false)}
+            >
+              <Text style={{ color: '#8E96FA' }}>아니요</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonYes]}
+              onPress={confirmCreateQuestion}
+            >
+              <Text style={{ color: '#FFF' }}>네</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+  
+
+  // 삭제
+  const handleDeleteQuestion = (questionId) => {
+    setDeleteQuestionId(questionId);
+    setShowDeleteConfirmModal(true);
+  };
+
+   const confirmDeleteQuestion = async () => {
+    if (!deleteQuestionId) return;
+
+    // 삭제 로직
     const token = await AsyncStorage.getItem('@user_token');
     const config = {
       method: 'delete',
-      url: `http://edm.japaneast.cloudapp.azure.com/api/asks/${questionId}/`,
-      headers: { 
-        'Authorization': `Bearer ${token}`,
-      }
+      url: `http://edm.japaneast.cloudapp.azure.com/api/asks/${deleteQuestionId}/`,
+      headers: { 'Authorization': `Bearer ${token}` },
     };
-  
+
     try {
       await axios(config);
-      // 성공적으로 삭제된 후의 처리. 예를 들어, 문의 목록을 다시 불러올 수 있습니다.
-      setShowModal(false);
-      // 문의 목록을 다시 불러오는 로직 추가 필요
+      const updatedQuestions = questions.filter(question => question.id !== deleteQuestionId);
+      setQuestions(updatedQuestions);
+      setShowModal(false); // 기존 모달 닫기
     } catch (error) {
       console.error(error);
       // 에러 처리 로직
     }
+
+    setShowDeleteConfirmModal(false); // 삭제 확인 모달 닫기
+    setDeleteQuestionId(null); // 삭제할 문의 ID 초기화
   };
+
+  // 삭제 확인 모달
+  const renderDeleteConfirmModal = () => (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={showDeleteConfirmModal}
+      onRequestClose={() => setShowDeleteConfirmModal(false)}
+    >
+      <View style={styles.modalOverlay}>
+        <View style={styles.modalView}>
+          <View style={{ alignSelf: 'flex-start' }}> 
+            <Text style={{textAlign:'left', fontSize:16, color:'black'}}>삭제하시겠습니까?</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonNo]}
+              onPress={() => setShowDeleteConfirmModal(false)}
+            >
+              <Text style={{ color: '#8E96FA' }}>아니요</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonYes]}
+              onPress={confirmDeleteQuestion}
+            >
+              <Text style={{ color: '#FFF' }}>네</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+
+  // 문의하기 모달창
   const renderQuestionModal = () => (
     <Modal
-      animationType="slide"
+      animationType="fade"
       transparent={true}
       visible={showQuestionModal}
       onRequestClose={() => setShowQuestionModal(false)}
     >
+      <View style={styles.modalOverlay}>
       <View style={styles.modalView}>
+        <TouchableOpacity
+          onPress={() => setShowQuestionModal(false)}
+          style={{
+            position: 'absolute',
+            top: 10,
+            right: 10,
+            padding: 10,
+            zIndex: 1 // 버튼이 다른 요소 위에 표시되도록 zIndex 설정
+          }}>
+          <Text>X</Text>
+        </TouchableOpacity>
         <TextInput
-          style={styles.input}
           placeholder="문의 제목"
           value={newQuestionTitle}
           onChangeText={setNewQuestionTitle}
         />
+
+        <View style={styles.separator} />
+
         <TextInput
           style={[styles.input, {height: 100}]}
           placeholder="문의 내용"
           value={newQuestionContent}
           onChangeText={setNewQuestionContent}
           multiline
+          textAlign="left"
         />
         <TouchableOpacity
           style={styles.submitButton}
           onPress={handleCreateQuestion}
         >
-          <Text>등록</Text>
+          <Text style={{color:'#fff'}}>등록</Text>
         </TouchableOpacity>
+      </View>
       </View>
     </Modal>
   );
+
   return (
     <View style={styles.container}>
       {/* 이전 버튼 */}
@@ -171,7 +324,7 @@ const QuestionScreen = () => {
         style={styles.createButton}
         onPress={() => setShowQuestionModal(true)}
       >
-        <Text>문의하기</Text>
+        <Text style={{color:'#fff'}}>문의하기</Text>
       </TouchableOpacity>
 
       {renderQuestionModal()}
@@ -192,6 +345,9 @@ const QuestionScreen = () => {
       />
 
       {renderModalContent()}
+      {renderAlertModal()}
+      {renderConfirmModal()}
+      {renderDeleteConfirmModal()}
     </View>
   );
 };
@@ -215,6 +371,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   modalView: {
     margin: 20,
     backgroundColor: "white",
@@ -228,13 +390,91 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
+    width:'80%',
   },
   closeButton: {
-    marginTop: 20,
-    backgroundColor: "#D7D4FF",
+    marginTop:20,
+    backgroundColor: '#8E86FA',
+    borderRadius: 40,
     padding: 10,
+    marginHorizontal: 10,
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  createButton:{
+    marginTop:20,
+    backgroundColor: '#8E86FA',
+    borderRadius: 40,
+    padding: 10,
+    marginHorizontal: 10,
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  submitButton:{
+    marginTop:20,
+    backgroundColor: '#8E86FA',
+    borderRadius: 40,
+    padding: 10,
+    marginHorizontal: 10,
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: '#ccc',
+    width: '100%',
+    marginVertical: 10,
+  },
+
+  input: {
+    width: '100%', // 폭 조정
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#eee', // 배경색 어둡게 조정
+  },
+
+  deleteButton:{
+    marginTop:20,
+    backgroundColor: 'red',
+    borderRadius: 40,
+    padding: 10,
+    marginHorizontal: 10,
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 20,
+    width:'100%',
+  },
+  button: {
+    padding: 10,
+    margin: 5,
     borderRadius: 10,
+  },
+  buttonNo: {
+    backgroundColor: '#FFF',
+    // 추가적인 스타일링
+  },
+  buttonYes: {
+    backgroundColor: '#8E86FA',
+    // 추가적인 스타일링
   },
 });
 
